a1792c06034c80922d103675d5871d783609a68f
Márcio Mingante <marciomvp17@gmail.com>
2024-01-29 11:06:39 -0300

    conclui requisito sete

--- src/7-search-products-by-brand.js
+++ src/7-search-products-by-brand.js
@@ -1,7 +1,21 @@
 const stockProducts = require('./data.json');
 
-const searchProductsByBrand = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductsByBrand = (name) => {
+  let productsList = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].brand === name) {
+      let product = {
+        description: '',
+        formattedPrice: '',
+      };
+      product.description = stockProducts[index].description;
+      product.formattedPrice = `R$ ${stockProducts[index].price}`;
+      productsList.push(product);
+    }
+  }
+
+  return productsList;
 };
 
 module.exports = { searchProductsByBrand };
