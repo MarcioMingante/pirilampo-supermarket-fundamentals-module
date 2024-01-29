71c3fb2dba5d773ed242dcf1f286c9280e1fbb33
Márcio Mingante <marciomvp17@gmail.com>
2024-01-29 10:03:19 -0300

    conclui requisito 4

--- src/4-low-stock-products.js
+++ src/4-low-stock-products.js
@@ -1,7 +1,15 @@
 const stockProducts = require('./data.json');
 
 const getLowStockProducts = () => {
-  // Desenvolva seu código dentro dessa função...
+  let lowStockProducts = [];
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].quantityInStock > 0 && stockProducts[index].quantityInStock <= 10) {
+      lowStockProducts.push(`${stockProducts[index].productName}: ${stockProducts[index].quantityInStock} unidades`);
+    }
+  }
+
+  return lowStockProducts;
 };
 
 module.exports = { getLowStockProducts };
