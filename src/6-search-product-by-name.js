23818ddd4c8c2b78935efeee5b6ae6821fe65020
Márcio Mingante <marciomvp17@gmail.com>
2024-01-29 10:33:44 -0300

    conclui requisito 6

--- src/6-search-product-by-name.js
+++ src/6-search-product-by-name.js
@@ -1,7 +1,20 @@
 const stockProducts = require('./data.json');
 
-const searchProductByName = () => {
-  // Desenvolva seu código dentro dessa função...
+const searchProductByName = (name) => {
+  let product = {
+    description: '',
+    formattedPrice: '',
+  };
+
+  for (let index = 0; index < stockProducts.length; index += 1) {
+    if (stockProducts[index].productName === name) {
+      product.description = stockProducts[index].description;
+      product.formattedPrice = `R$ ${stockProducts[index].price}`;
+      return product;
+    }
+  }
+
+  return null;
 };
 
 module.exports = { searchProductByName };
